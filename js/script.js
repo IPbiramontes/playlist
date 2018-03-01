0d272ef4fba935fffc0c0b487a283773461be98e
Isabela Biramontes <ibiramontes.student@hsfi.us>
2018-03-01 23:19:43 +0000

    struggglng on s=displaying songs

--- js/script.js
+++ js/script.js
@@ -17,42 +17,57 @@
 
 // Songs
 var mySong = {
-	"title":"24K Magic",
-	"artist":"Bruno Mars",
-	"mp3-url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
-	"image-url":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
-}
+	"title":"Wave",
+	"artist":"Justin Timberlake",
+	"mp3-url":"https://open.spotify.com/track/1a8051h021Rfl01OhAjjV6",
+	"image-url":"css/motw.png",
+};
 
 var myPlayList = [
 	{
-		"title":"24K Magic",
+		"title":"Wave",
+		"artist":"Justin Timberlake",
+		"mp3-url":"https://open.spotify.com/track/1a8051h021Rfl01OhAjjV6",
+		"image-url":"css/motw.png",
+	},
+	{
+		"title":"Perm",
 		"artist":"Bruno Mars",
-		"mp3-url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
+		"mp3-url":"https://open.spotify.com/track/1I6pKIyaBp4OebTGLJpCCC",
 		"image-url":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
 	},
 	{
-		"title":"Sir Duke",
-		"artist":"Stevie Wonder",
-		"mp3-url":"https://open.spotify.com/track/2udw7RDkldLFIPG9WYdVtT",
-		"image-url":"https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Songs_in_the_key_of_life.jpg/220px-Songs_in_the_key_of_life.jpg",
+		"title":"After the Storm (ft. Tyler the Creator & Bootsy Collins)",
+		"artist":"Kali Uchis",
+		"mp3-url":"https://open.spotify.com/track/1gmarFWgSwb4SWlmqDjWka",
+		"image-url":"css/ats.jpeg",
 	},
 	{
-		"title":"Sorry",
-		"artist":"Justin Bieber",
-		"mp3-url":"https://open.spotify.com/track/09CtPGIpYB4BrO8qb1RGsF",
-		"image-url":"http://assets-s3.usmagazine.com/uploads/assets/articles/93827-justin-biebers-sorry-choreographer-spills-video-style-secrets-parris-goebel/1445638548_justin-bieber-sorry-dancers-zoom.jpg",
+		
+		"title":"Honey",
+		"artist":"Kehlani",
+		"mp3-url":"https://open.spotify.com/track/5QTdOvIF2ehBMZpSIIGzIo",
+		"image-url":"css/honey.jpg",
 	}
 
-]
+];
+	
+	var WaveArtist = myPlayList[0].artist;
+	var WaveUrl = myPlayList[0].mp3-url;
+	var WaveImg = myPlayList[0].image-url;
 
 
 
+
+/* global*/
 // DOCUMENT READY FUNCTION
 $( document ).ready(function() {
-  
-
-
-
+	var WaveTitle = myPlayList[0].title;
+	$("body").append("<h3>"+ WaveTitle +"</h3>");
+	$("body").append("<p>"+myPlayList[0].artist+"</p>");
+	$("body").append("<p>"+myPlayList[0].artist+"</p>");
+console.log(WaveTitle);
+ 
 });
 
 function displayList(){
